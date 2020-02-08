880a6e7ea428eb3b61982af6fc07d5adfe839104
Khoa Do <khoado.dk@gmail.com>
2020-02-07 19:24:11 -0500

    Changed the env var

--- client/config.js
+++ client/config.js
@@ -1,17 +1,20 @@
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
+// import getConfig from 'next/config';
+// const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION
-  ? publicRuntimeConfig.API_PRODUCTION
-  : publicRuntimeConfig.API_DEVELOPMENT;
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const API =
+  process.env.NODE_ENV === 'development'
+    ? process.env.API_DEVELOPMENT
+    : process.env.API_PRODUCTION;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
-  ? publicRuntimeConfig.DOMAIN_PRODUCTION
-  : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+export const DOMAIN =
+  process.env.NODE_ENV === 'development'
+    ? process.env.DOMAIN_DEVELOPMENT
+    : process.env.DOMAIN_PRODUCTION;
 
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const APP_NAME = process.env.APP_NAME;
 
-export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
+export const FB_APP_ID = process.env.FB_APP_ID;
 
-export const SENDGRID_API = publicRuntimeConfig.SENDGRID_API;
+export const DISQUS_SHORTNAME = process.env.DISQUS_SHORTNAME;
+
+export const SENDGRID_API = process.env.SENDGRID_API;
